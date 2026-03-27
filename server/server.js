d3b122415402be91ce2a73e9052d6cfadf246f75
lydat1502 <143302026+Ly-Dat@users.noreply.github.com>
2026-03-27 09:02:31 +0700

    fix

--- server/server.js
+++ server/server.js
@@ -17,22 +17,16 @@ app.use('/api/income', incomeRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/expenses', expenseRoutes);
 
-// test route (nên có)
-app.get('/', (req, res) => {
-  res.send('API is running 🚀');
-});
-
 // MongoDB connection
 const uri = process.env.MONGO_URI;
-
 mongoose
-  .connect(uri)
+  .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB successfully!'))
   .catch((err) => console.error('Failed to connect to MongoDB:', err.message));
 
 // start server
-const PORT = process.env.PORT || 3000;
-
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
